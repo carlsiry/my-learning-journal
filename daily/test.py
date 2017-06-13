23df2df24ec74628c806f6b88e3904d67e7676be
Carlsiry <chenkansheng0@163.com>
2017-06-13 22:15:36 +0800

    Updates

--- daily/test.py
+++
@@ -1,9 +0,0 @@
-#coding=utf-8
-name = 'carson'
-age = 18
-print('name: %s, age: %d'%(name,age))
-
-place = input('And where are you want to go?') # must string type, such as 'Beijing'
-# place = raw_input('And where are you want to go?') # raw input, such as Beijing
-
-print('ok, we will go to %s, now!'%place)
